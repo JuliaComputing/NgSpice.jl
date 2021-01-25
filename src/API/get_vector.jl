e22b055e841044a2969c7a47cd97e4a1da93b5dc
Venkateshprasad <vensadk@gmail.com>
2021-01-25 20:17:47 +0530

    Handling vectors

--- src/API/get_vector.jl
+++ src/API/get_vector.jl
@@ -1,4 +1,4 @@
-function get_vector_info(vecname, maxlen=Inf)
+function get_vector_info(vecname, maxlen=Int(maxintfloat()))
     vec = ngGet_Vec_Info(vecname)
     vec != C_NULL || throw("Vector $(vecname) not found")
     vecinfo = unsafe_load(vec)
@@ -13,4 +13,43 @@ function get_vector_info(vecname, maxlen=Inf)
     else
         error("Unknown vector type")
     end
+end
+
+curplot() = ngSpice_CurPlot()
+
+function allvecs()
+    curplot = ngSpice_CurPlot();
+    curplot != C_NULL || throw("No current plots")
+    allplots = ngSpice_AllVecs(curplot)
+    ret = []
+    plot = allplots
+    while plot != C_NULL
+        ret = [ret; unsafe_string(unsafe_load(plot))] #see if this is efficient
+        plot += 1
+    end
+    return ret
+end #revisit
+
+function getvec(name, maxlen=Int(maxintfloat()))
+    get_vector_info(name, maxlen) 
+end
+
+function getrealvec(name, maxlen=Int(maxintfloat()))
+    _, data = get_vector_info(name, maxlen)
+    real(data)
+end
+
+function getimagvec(name, maxlen=Int(maxintfloat()))
+    _, data = get_vector_info(name, maxlen)
+    imag(data)
+end
+
+function getmagnitudevec(name, maxlen=Int(maxintfloat()))
+    _, data = get_vector_info(name, maxlen)
+    abs.(data)
+end
+
+function getphasevec(name, maxlen=Int(maxintfloat))
+    _, data = get_vector_info(name, maxlen)
+    angle.(data)
 end
--- src/API/running.jl
+++ src/API/running.jl
@@ -2,14 +2,15 @@ cmd(command) = ngSpice_Command(command) #should typeof(command) be restricted
 
 init()      = (pvoid = convert(Ptr{Nothing}, 0); 
                 ngSpice_Init(psendchar, psendstat, pcontrolledexit, 
-                psenddata, psendinitdata, pbgthread, pvoid))
+                pvoid, pvoid,  pbgthread, pvoid))
+                #psenddata, psendinitdata, pbgthread, pvoid))
 
 isrunning() = ngSpice_running()
 
-simrun()    = (init(); cmd(:run))
+ngrun()     = (init(); cmd(:run))
 bgrun()     = (init(); cmd(:bg_run))
 
-simhalt()   = cmd(:halt)
+ngstop()    = cmd(:stop)
 bghalt()    = cmd(:bg_halt) 
 
 reset()     = cmd(:reset)
