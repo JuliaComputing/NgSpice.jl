fcd0aab11241b0286b36a75542d1da14ea4d8648
Venkateshprasad <vensadk@gmail.com>
2021-03-22 22:15:56 +0530

    Don't init with run

--- src/API/running.jl
+++ src/API/running.jl
@@ -2,13 +2,12 @@ cmd(command) = ngSpice_Command(command) #should typeof(command) be restricted
 
 init()      = (pvoid = convert(Ptr{Nothing}, 0); 
                 ngSpice_Init(psendchar, psendstat, pcontrolledexit, 
-                pvoid, pvoid,  pbgthread, pvoid))
-                #psenddata, psendinitdata, pbgthread, pvoid))
+                psenddata, psendinitdata, pbgthread, pvoid))
 
 # isrunning() = ngSpice_running() # always returns 0
 
-run()     = (init(); cmd("run"))
-bgrun()   = (init(); cmd("bg_run"))
+run()     = cmd("run")
+bgrun()   = cmd("bg_run")
 
 stop()    = cmd("stop")
 bghalt()  = cmd("bg_halt") 
