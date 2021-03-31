b8f213dd225b3e09eb09cec16cbf45aee1a8d618
Venkateshprasad <vensadk@gmail.com>
2021-03-31 20:38:40 +0530

    - test the friend functions - allow j^1.5

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,10 @@ using Test
 rootdir = joinpath(@__DIR__, "..")
 netpath = joinpath(rootdir, "inputs", "mosfet.cir")
 
-@testset "interface" begin
+@testset "interface functions" begin
     include("ngspice_api.jl")
+end
+
+@testset "friend functions" begin
+    include("wrapper_api.jl")
 end
