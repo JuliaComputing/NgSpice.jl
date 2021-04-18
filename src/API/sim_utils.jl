e659b536c3169d8539791e4199819268f26f7d5c
Venkateshprasad <vensadk@gmail.com>
2021-04-18 19:28:45 +0530

    load_netlist will deal only with netlists and not netpaths
    - fixes the test too

--- src/API/sim_utils.jl
+++ src/API/sim_utils.jl
@@ -3,16 +3,13 @@ function load_netlist(netlist::AbstractArray{T}) where T <: AbstractString
     t = netlist |> ngSpice_Circ
 end
 
-function load_netlist(net::AbstractString)
-    isfile(net) && (netlist = readlines(net);
-        load_netlist(netlist);
-        return)
+function load_netlist(netlist::AbstractString)
     # The strings in Julia always use \n instead of \r \r\n
     # (even if the editor does).
     # But if the circuit-string has \r in it,
     # uncomment the following line
-    # occursin("\r", net) && replace(net, "\r"=>"")
-    load_netlist(split(net, "\n"))
+    # occursin("\r", netlist) && replace(netlist, "\r"=>"")
+    load_netlist(split(netlist, "\n"))
 end
 
 source(netpath) = cmd("source $netpath")
