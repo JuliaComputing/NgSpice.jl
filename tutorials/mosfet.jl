485fa2f0732158850c1535eccb534dcc18be66bf
Glen Hertz <glen.hertz@gmail.com>
2021-09-01 16:57:30 -0400

    Fix tutorials

--- tutorials/mosfet.jl
+++ tutorials/mosfet.jl
@@ -1,7 +1,8 @@
 using NgSpice
+using Plots
 n = NgSpice
 
-netpath = joinpath(@__DIR__, "inputs", "mosfet.cir")
+netpath = joinpath(@__DIR__, "..", "inputs", "mosfet.cir") |> normpath
 
 n.init()
 n.source(netpath)
@@ -10,8 +11,10 @@ n.display()        # Returns a dataframe of current vectors
 n.listallvecs()    # Lists vectors of both active and inactive plots
 n.curplot()        # Current active plot
 n.listcurvecs()    # Vectors in current active plot
-n.getvec("emit")   # Returns vector info of `emit` vector 
-plot(n.graph, "time", ["emit", "vcc"], "Title of the plot")
 # Vector type can be specified by sending it as a parameter
 # plot(n.graph, n.getimagvec, "time", ["emit", "vcc"], "Title of the plot")
+sweep = n.getvec("time")[end]   # Returns vector info of `time` vector 
+emit = n.getvec("emit")[end]   # Returns vector info of `emit` vector 
+vcc = n.getvec("vcc")[end]   # Returns vector info of `vcc` vector 
+plot(sweep, [emit vcc], label=["emit" "vcc"], title="Title of the plot")
 n.exit()           # Runs "unset askquit" of Ngspice
